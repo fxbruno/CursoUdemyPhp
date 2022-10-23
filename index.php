04aeac64b17f4e9a8cb1dd4b484e0975ca8774f9
Bruno Felix de Oliveira <fxbruno@gmail.com>
2022-10-23 17:01:23 -0300

    atualização até desafio variável

--- index.php
+++ index.php
@@ -89,6 +89,21 @@
                         </li>
                     </ul>
                 </div>
+                <div class="modulo azul">
+                    <h3>Variáveis</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=basico">
+                                Variáveis
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=desafio_equacao">
+                                Desafio Equação
+                            </a>
+                        </li>
+                    <ul>
+                    </div>        
             </nav>
         </div>
     </main>
