9ced79586fa38ac56a3057cd56a67b584efba0e9
Bruno Felix de Oliveira <fxbruno@gmail.com>
2022-10-25 22:49:42 -0300

    adicionado aula de atribuição

---
+++ variaveis/atribuicoes.php
@@ -0,0 +1,52 @@
+<div class="titulo"></div>
+
+<?php
+
+$tittle = 'atribuições';
+$numero = 10;
+echo '<br>'. $numero; 
+$numero = $numero -3;
+echo '<br>'. $numero; 
+$numero = $numero + 1.5;
+echo '<br>'. $numero; 
+$numero--;
+echo '<br>'. $numero; 
+--$numero;
+echo '<br>'. $numero; 
+++$numero;
+echo '<br>'. $numero; 
+$numero++;
+echo '<br>'. $numero; 
+$numero =20;
+echo '<br>'. $numero; 
+$numero -= 5;
+echo '<br>'. $numero; 
+$numero +=5;
+echo '<br>'. $numero; 
+$numero *=10;
+echo '<br>'. $numero; 
+$numero /=2;
+echo '<br>'. $numero; 
+$numero %=6;
+echo '<br>'. $numero; 
+$numero **=4;
+echo '<br>'. $numero; 
+$numero .=4;//apenas concatenação!
+echo '<br>'. $numero;
+$texto = 'Esse é um texto'; 
+echo '<br>'. $texto;
+$texto = $texto . ' qualquer';
+echo '<br>'. $texto;
+$texto .=  ' de verdade';
+echo '<br>'. $texto;
+
+//$variavelInexistente ='valor inexistente';
+echo '<br>'. $variavelInexistente;
+$valor= $variavelInexistente ?? 'valor dafault';
+
+echo '<br>'. $valor;
+
+
+
+
+
